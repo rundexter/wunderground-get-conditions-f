7ae6b845d2f287e312835184c975294df34d9fe4
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-16 14:26:49 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ module.exports = {
             return false;
         } else {
 
-            request.defaults({
+            request = request.defaults({
                 baseUrl: apiURL.concat(_(dexter.environment('wunderground_api_key')).toString().trim())
             });
 
